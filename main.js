0ccc06e20c18731f155dd3f56cd8a47ae8a0ae66
Cris-Rider <cristobalgl46@gmail.cl>
2020-09-03 18:22:12 -0400

    Agregando elementos html con js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 window.onload=()=>{
 const parrafo= document.getElementById('text')//consigue los elementos de html segun su id
-console.log(parrafo.innerText);
+parrafo.innerText = 'Texto actualizado'//podemos cambiar contenido de etiquetas
+parrafo.innerHTML = '<li>elemento 1</li><li>elemento 2</li>'//podemos agregar elementos html a otro elemento html
 }
