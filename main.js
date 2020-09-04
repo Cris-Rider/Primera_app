06e880d85e45a75d1b6f1f7ba851bbff417b8bba
Cris-Rider <cristobalgl46@gmail.cl>
2020-09-03 20:11:20 -0400

    Inspeccionando elementos de to do list

--- main.js
+++ main.js
@@ -11,6 +11,19 @@ window.onload=()=>{
         const todolist = document.getElementById('todo-list');
         const todosTemplate = todos.map(t => '<li>'+t+'</li>');
         todolist.innerHTML = todosTemplate.join('');
-        
+        //INSPECCIONANDO ELEMENTOS DE LA TO DO LIST PRIMERA FORMA
+        //document.querySelectorAll('#todo-list')
+        //const todolist = document.querySelectorAll('#todo-list')
+        //Array.from(todolist)
+        //const elementos = document.querySelectorAll('#todolist li')
+        //Array.from(elementos)
+        //elementos.forEach(x => console.log(x))
+        const elementos = document.querySelectorAll('#todo-list li')
+        elementos.forEach((elemento, i)=>{
+            elemento.addEventListener('click',() => {
+                console.log(elemento,i)
+            })
+        })
+
     }
 }
