c78b887b910f8b17d7a02c19f0f4b779d67c5d7b
Cris-Rider <cristobalgl46@gmail.cl>
2020-09-03 20:29:37 -0400

    Eliminando elementos recursivamente

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 const todos=[];
 
+const render = () => {
+    const todolist = document.getElementById('todo-list');
+    const todosTemplate = todos.map(t=>'<li>'+t+'</li>');
+    todolist.innerHTML = todosTemplate.join('');
+    const elementos = document.querySelectorAll('#todo-list li') 
+    elementos.forEach((elemento, i)=>{
+        elemento.addEventListener('click',() => {
+            elemento.parentNode.removeChild(elemento);
+            todos.splice(i, 1)
+            render()
+        })
+    })
+}
 window.onload=()=>{
     const form= document.getElementById('todo-form');
     form.onsubmit = (e) => {
-        e.preventDefault();//previene el refresco de ventana que ocasiona submit
+        e.preventDefault();
         const todo = document.getElementById('todo');
         const todoText = todo.value;
         todo.value='';
         todos.push(todoText);
-        const todolist = document.getElementById('todo-list');
-        const todosTemplate = todos.map(t=>'<li>'+t+'</li>');
-        todolist.innerHTML = todosTemplate.join('');
-        const elementos = document.querySelectorAll('#todo-list li')
-        //ELIMINANDO ELEMENTOS 
-        elementos.forEach((elemento, i)=>{
-            elemento.addEventListener('click',() => {//al registrar el evento de click, addEventListener imprimira el elemento en la consola
-                elemento.parentNode.removeChild(elemento);//elimina elementos de la lista
-                todos.splice(i, 1)
-                console.log(todos,i)
-            })
-        })
-
+        render()
     }
 }
